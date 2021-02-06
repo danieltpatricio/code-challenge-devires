0d722aa60cb3af9e86a5d571b27e724dfdb263fa
Daniel Patricio <danieltp123@gmail.com>
2021-02-06 04:06:14 -0300

    creating Todo Store

--- src/store/ducks/index.ts
+++ src/store/ducks/index.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import exampleReducer from './example.duck';
+import todoReducer from './todos';
 
 export default combineReducers({
   exampleReducer,
+  todoReducer,
 });
---
+++ src/store/ducks/todos.ts
@@ -0,0 +1,51 @@
+import { createSlice, PayloadAction, Dispatch } from '@reduxjs/toolkit';
+import { DefaultState } from 'models/redux';
+import { ITodo } from 'models/todos';
+import api from 'services/api';
+import { RootState } from 'store';
+
+const INITIAL_STATE: DefaultState<ITodo[]> = {
+  data: [],
+  loading: false,
+  error: undefined,
+};
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState: INITIAL_STATE,
+  reducers: {
+    setTodos: (state, { payload }: PayloadAction<ITodo[]>) => ({
+      ...state,
+      data: payload,
+    }),
+    addTodo: (state, { payload }: PayloadAction<ITodo>) => ({
+      ...state,
+      data: [...state.data, payload],
+    }),
+    removeTodo: (state, { payload: id }: PayloadAction<number>) => ({
+      ...state,
+      data: state.data.filter((i) => i.id !== id),
+    }),
+  },
+});
+
+export const { setTodos, addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
+
+export const getTodo = () => async (dispatch: Dispatch) => {
+  const { data } = await api.get<ITodo[]>('/todos');
+  dispatch(setTodos(data));
+};
+
+export const saveTodo = () => async (dispatch: Dispatch, todo: ITodo) => {
+  const { data } = await api.post<ITodo>('/todos', todo);
+  dispatch(addTodo(data));
+};
+
+export const deleteTodo = () => async (dispatch: Dispatch, id: number) => {
+  await api.delete<ITodo>(`/todos/${id}`);
+  dispatch(removeTodo(id));
+};
+
+export const selectTodosState = (state: RootState) => state.todoReducer;
