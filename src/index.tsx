9b687f99b911106f6311bc5016e6619881e68d62
Daniel Patricio <danieltp123@gmail.com>
2021-02-06 04:04:36 -0300

    creating initial Theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'store';
 import App from './app/App';
+import { ThemeProvider } from 'styled-components';
+import theme from 'theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#212121',
+    secondary: '#909090',
+  },
+  borderRadius: '10px',
+};
+
+export default theme;
